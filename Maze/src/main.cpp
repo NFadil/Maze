f01f2715b5742d3b26dab9cd3577962d0a62f5a7
Aakash G. Shah <ashah@isengard.mines.edu>
2012-11-19 10:27:09 -0700

    Finished

--- Maze/src/main.cpp
+++ Maze/src/main.cpp
@@ -13,36 +13,42 @@ using namespace std;
 
 
 struct ParentInfo {
-	int parent;
+		ParentInfo() :
+				parent(-1) {}
+		int parent;
 };
 
-typedef adjacency_list<listS, vecS, undirectedS, ParentInfo> MyGraph;
+typedef adjacency_list<listS, vecS, directedS, ParentInfo> MyGraph;
 typedef graph_traits<MyGraph>::vertex_descriptor MyVertex;
 typedef graph_traits<MyGraph>::edge_descriptor MyEdge;
 
 class GraphEdge {
-  public:
-    GraphEdge(int v, int v2, int ec, char c, char t) :
-      vertex(v),
-      vertex2(v2),
-      edgeCount(ec),
-      color(c),
-      type(t),
-	  parent(-1) {}
+		public:
+				GraphEdge() :
+						vertex(-1), vertex2(-1), edgeCount(-1), color('E'), type('E'), parent(-1) {}
 
-   int getStartVertex() { return vertex; }
-   int getEndVertex() { return vertex2; }
-   int getEdgeCount() { return edgeCount; }
-   char getColor() { return color; }
-   char getType() { return type; }
-   int getParent() { return parent; }
-   void setParent(int e) { parent = e; }
+				GraphEdge(int v, int v2, int ec, char c, char t) :
+						vertex(v),
+						vertex2(v2),
+						edgeCount(ec),
+						color(c),
+						type(t),
+						parent(-1) {}
 
-  private:
-    int vertex, vertex2, edgeCount;
-    char color, type;
-	int parent;
+				int getStartVertex() { return vertex; }
+				int getEndVertex() { return vertex2; }
+				int getEdgeCount() const { return edgeCount; }
+				char getColor() { return color; }
+				char getType() { return type; }
+				int getParent() { return parent; }
+				void setParent(int e) { parent = e; }
+
+		private:
+				int vertex, vertex2, edgeCount;
+				char color, type;
+				int parent;
 };
+
 
 typedef map<int, char> ReverseVertexMap;
 typedef map<char, int> VertexMap;
@@ -51,24 +57,25 @@ typedef vector<GraphEdge> VectorEdges;
 class MyVisitor : public default_dfs_visitor {
 
 
-	public:
-    MyVisitor(MyGraph& g) :
-		graph(g) {}
-	void tree_edge(MyEdge e, const MyGraph& g) {
-		int sourceIndex = source(e, g);
-		int targetIndex = target(e, g);
-		
-		cout << "Setting the parent of " << targetIndex << " to " << sourceIndex << endl;
+		public:
+				MyVisitor(MyGraph* g) :
+						graph(g) {}
+				void tree_edge(MyEdge e, const MyGraph& g) {
+						int sourceIndex = source(e, g);
+						int targetIndex = target(e, g);
 
-		graph[targetIndex].parent = sourceIndex;
-	}
-	
-	int getParent(int index) {
-		return graph[3].parent;
-	}
+						if ((*graph)[targetIndex].parent == -1) {
+								(*graph)[targetIndex].parent = sourceIndex;
+						}
 
-  private:
-  	MyGraph graph;
+				}
+
+				int getParent(int index) {
+						return (*graph)[index].parent;
+				}
+
+		private:
+				MyGraph* graph;
 };
 
 const string FILENAME = "data/input.txt";
@@ -78,50 +85,71 @@ int readInputFile(MyGraph& g, VertexMap& vertices, ReverseVertexMap& reverseVert
 void processAndAddEdges(MyGraph& g, VertexMap& vertices, VectorEdges& inputEdges);
 bool compareTwoEdges(GraphEdge e, GraphEdge e2);
 int mapEdgeIndex(int index, bool isLeavingStart);
-
+int reverseMapEdgeIndex(int index);
 
 int main() {
-	ReverseVertexMap reverseVertices;
-	VertexMap vertices;
-	VectorEdges inputEdges;
-	MyGraph g;
-	int numEdges = readInputFile(g, vertices, reverseVertices, inputEdges);
-	processAndAddEdges(g, vertices, inputEdges); 
-	MyVisitor vis(g);
-	depth_first_search(g, boost::visitor(vis));
+		ReverseVertexMap reverseVertices;
+		VertexMap vertices;
+		VectorEdges inputEdges;
+		MyGraph g;
+		int numEdges = readInputFile(g, vertices, reverseVertices, inputEdges);
+		processAndAddEdges(g, vertices, inputEdges); 
+		MyVisitor vis(&g);
+		cout << "\n\nStarting DFS\n\n";
+		depth_first_search(g,  boost::visitor(vis));
 
-	cout << "Parent: " << vis.getParent(85) << endl;
-	return 0;
+		stack<char> path;
+		int parent = numEdges;
+		while (parent != -1) {
+				int index = reverseMapEdgeIndex(parent);
+				GraphEdge node = inputEdges.at(index);
+				if (parent % 2 == 0)
+					path.push(reverseVertices[node.getEndVertex()]);
+				else
+					path.push(reverseVertices[node.getStartVertex()]);
+
+				parent = vis.getParent(parent);
+				if (parent == -1)
+						path.push(reverseVertices[node.getStartVertex()]);
+
+		}
+		while (!path.empty()) {
+				cout << path.top() << " -> ";
+				path.pop();
+		}
+
+		cout << "\n\n";
+
+		ofstream log("log.txt");
+		write_graphviz(log, g);
+		log.close();
+		return 0;
 }
 
 int readInputFile(MyGraph& g, VertexMap& vertices, ReverseVertexMap& reverseVertices, VectorEdges& inputEdges) {
-	ifstream fileHandle(FILENAME.c_str());
-	if (!fileHandle) {
-		cerr << "Error opening input file\n";
-		exit(1);
-	}
-	int vertexCount = 0;
-  int edgeCount = 0;
-	int numVillages, numEdges;
-	fileHandle >> numVillages >> numEdges;
-	for (int i = 0; i < numEdges; ++i) {
-		char v1, v2, color, type;
-		fileHandle >> v1 >> v2 >> color >> type;
+		ifstream fileHandle(FILENAME.c_str());
+		if (!fileHandle) {
+				cerr << "Error opening input file\n";
+				exit(1);
+		}
+		int vertexCount = 0;
+		int edgeCount = 0;
+		int numVillages, numEdges;
+		fileHandle >> numVillages >> numEdges;
+		for (int i = 0; i < numEdges; ++i) {
+				char v1, v2, color, type;
+				fileHandle >> v1 >> v2 >> color >> type;
 
-		//Check if we have a vertext number for each line read
-		if (vertices.find(v1) == vertices.end()) {
-			vertices[v1] = vertexCount++;
-      reverseVertices[vertexCount] = v1;
-    }
-		if (vertices.find(v2) == vertices.end()) {
-			vertices[v2] = vertexCount++;
-      reverseVertices[vertexCount] = v2;
-    }
-    
-//    cout << "Recording edge from " << v1 << " to " << v2 << " index: " << edgeCount << endl;
-    inputEdges.push_back(GraphEdge(vertices[v1], vertices[v2], edgeCount++, color, type));
-		//create the edge between the two vertices
-		//add_edge(vertices[v1], vertices[v2], g);
+				//Check if we have a vertext number for each line read
+				if (vertices.find(v1) == vertices.end()) {
+						vertices[v1] = vertexCount;
+						reverseVertices[vertexCount++] = v1;
+				}
+				if (vertices.find(v2) == vertices.end()) {
+						vertices[v2] = vertexCount;
+						reverseVertices[vertexCount++] = v2;
+				}
+		    	inputEdges.push_back( GraphEdge(vertices[v1], vertices[v2], edgeCount++, color, type) );
 
 	}
 
@@ -146,24 +174,24 @@ void processAndAddEdges(MyGraph& g, VertexMap& vertices, VectorEdges& inputEdges
           
           //map the start node (which is traversing backwards) to the end node (which you are traversing normally)
           add_edge(mapEdgeIndex(startNode.getEdgeCount(), false), mapEdgeIndex(endNode.getEdgeCount(), true), g);
-          /*
-          cout << "Adding a backwards edge ";
-          cout << "(" << mapEdgeIndex(startNode.getEdgeCount(), true) << ", " << mapEdgeIndex(endNode.getEdgeCount(), true) << ")\n";
-          */
+          
         } else if (startNode.getEndVertex() == endNode.getStartVertex() &&
           compareTwoEdges(startNode, endNode)) {
           //map the start node and end node; both of which are traversed normally
           add_edge(mapEdgeIndex(startNode.getEdgeCount(), true), mapEdgeIndex(endNode.getEdgeCount(), true), g);
-          /*
-          cout << "Adding a normal edge ";
-          cout << "(" << mapEdgeIndex(startNode.getEdgeCount(), true) << ", " << mapEdgeIndex(endNode.getEdgeCount(), true) << ")\n";
-          */
-        }
+          add_edge(mapEdgeIndex(endNode.getEdgeCount(), false), mapEdgeIndex(startNode.getEdgeCount(), false), g);  
+          
+        } else if (startNode.getEndVertex() == endNode.getEndVertex() && compareTwoEdges(startNode, endNode)) {
+			add_edge(mapEdgeIndex(startNode.getEdgeCount(), true), mapEdgeIndex(endNode.getEdgeCount(), false), g);
+			add_edge(mapEdgeIndex(endNode.getEdgeCount(), true), mapEdgeIndex(startNode.getEdgeCount(), false), g);
+
+		}
 
       }
     }
   }
 }
+
 
 bool compareTwoEdges(GraphEdge e, GraphEdge e2) {
   if (e.getColor() == e2.getColor() ||
